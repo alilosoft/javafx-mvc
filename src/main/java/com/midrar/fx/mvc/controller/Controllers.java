1c7d205b74cd91401b5e41c032b70adf641dea42
@lilo$oft <fellahi.ali@gmail.com>
2017-10-19 23:35:16 +0100

    Adds Controllers.java class This class will be responsible for controllers instantiation. It has a default controller factory impl, but the client can provide its own factory impl if needed.

---
+++ src/main/java/com/midrar/fx/mvc/controller/Controllers.java
@@ -0,0 +1,21 @@
+package com.midrar.fx.mvc.controller;
+
+public class Controllers {
+    private Controllers() {} // hide default constructor
+
+    private static ControllerFactory _controllerFactory = ControllerFactory.reflectionFactory();
+
+    /**
+     * If you want to use a custom controller factory other then the default implementation
+     * -witch uses reflection- then use this method to provide your {@link ControllerFactory} impl.
+     * @param controllerFactory: a {@link ControllerFactory} implementation
+     */
+    public static void init(ControllerFactory controllerFactory) {
+        _controllerFactory = controllerFactory;
+    }
+
+    public static <T> T create(Class<T> clazz){
+        return _controllerFactory.create(clazz);
+    }
+
+}
