79757edcf16251d7af9b06357a4fcd034c60b9cc
@lilo$oft <fellahi.ali@gmail.com>
2017-10-20 01:25:45 +0100

    Implements new View methods

--- src/main/java/com/midrar/fx/mvc/view/ParentView.java
+++ src/main/java/com/midrar/fx/mvc/view/ParentView.java
@@ -26,6 +26,9 @@
  */
 package com.midrar.fx.mvc.view;
 
+import com.midrar.fx.mvc.controller.Controllers;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Tab;
@@ -44,47 +47,48 @@ import static com.midrar.fx.mvc.utils.Asserts.assertParameterNotNull;
  */
 @Data
 @Setter(AccessLevel.NONE)
+@ToString(of = {"controller", "rootNode"})
 abstract class ParentView<T> implements View<T>{
     private T controller;
     @NonNull private Parent rootNode;
+    private Optional<ResourceBundle> resourceBundle = Optional.empty();
     private String title = "";
     private List<Image> icons = Collections.EMPTY_LIST;
     private List<String> cssUrls = Collections.EMPTY_LIST;
     private ViewContext viewContext;
     protected AnnotationsParser annotationsParser;
 
-    ParentView(T _controller) {
-        assertParameterNotNull(_controller, "controller");
-        controller = _controller;
-        annotationsParser = new AnnotationsParser(controller.getClass());
-        rootNode = loadRoot(controller);
+    ParentView(Class<T> controllerClass) {
+        assertParameterNotNull(controllerClass, "controllerClass");
+        controller = Controllers.create(controllerClass);
+        annotationsParser = new AnnotationsParser(controllerClass);
+        resourceBundle = annotationsParser.resourceBundle();
         title = annotationsParser.title();
         icons = annotationsParser.icons();
         cssUrls = annotationsParser.cssUrls();
+        rootNode = loadRoot();
     }
 
     /**
-     * Load the root node hierarchy of this {@link ParentView}.
-     * @param controller
-     * @return a {@link Parent} Node represented by this {@link ParentView}.
+     * Load the root node hierarchy of this {@link View}.
+     * @return a {@link Parent} Node represented by this {@link View}.
      */
-    private Parent loadRoot(Object controller) {
+    private Parent loadRoot() {
         FXMLLoader fxmlLoader = new FXMLLoader();
         if (annotationsParser.isControllerInFxml()) {
-            // if the controller class is defined in the .view file using 'fx:controller'
+            // if the controller class is defined in the .fxml file using 'fx:controller'
             // then make sure that the controller factory used by fxmlLoader will return
             // the same controller instance of the ParentView  being initialized.
             fxmlLoader.setControllerFactory(c -> controller);
         } else {
-            // if the controller class is not defined in the .view file then pass the controller instance
+            // if the controller class is not defined in the .fxml file then pass the controller instance
             // of the ParentView being initialized to fxmlLoader.
             fxmlLoader.setController(controller);
         }
-        // parse the .view file url defined by @FXController
+        // parse the .fxml file url defined by @FXController
         URL fxmlUrl = annotationsParser.fxmlFileUrl();
         fxmlLoader.setLocation(fxmlUrl);
-        // parse and set the resource bundle if defined by @FXController
-        Optional<ResourceBundle> resourceBundle = annotationsParser.resourceBundle();
+        // set fxmlLoader resource bundle if present
         resourceBundle.ifPresent(fxmlLoader::setResources);
         try {
             return fxmlLoader.load();
@@ -118,6 +122,24 @@ abstract class ParentView<T> implements View<T>{
         //icon.ifPresent(i -> tab.setGraphic(new ImageView(i)));
         tabPane.getStylesheets().addAll(cssUrls);
         tabPane.getTabs().add(tab);
+    }
+
+    protected Optional<EventHandler<Event>> showEventHandler = Optional.empty();
+    @Override
+    public void setOnShow(EventHandler<Event> handler) {
+        showEventHandler = Optional.of(handler);
+    }
+
+    protected Optional<EventHandler<Event>> hideEventHandler = Optional.empty();
+    @Override
+    public void setOnHide(EventHandler<Event> handler) {
+        hideEventHandler = Optional.of(handler);
+    }
+
+    protected Optional<EventHandler<Event>> hideRequestEventHandler = Optional.empty();
+    @Override
+    public void setOnHideRequest(EventHandler<Event> handler) {
+        hideRequestEventHandler = Optional.of(handler);
     }
 
 }
