5a4494c889aa68a356dd9dd1570e05953c6186a1
@lilo$oft <fellahi.ali@gmail.com>
2017-10-20 01:25:08 +0100

    adds setOnShow, setOnHide, setOnHideRequest

--- src/main/java/com/midrar/fx/mvc/view/View.java
+++ src/main/java/com/midrar/fx/mvc/view/View.java
@@ -1,8 +1,14 @@
 package com.midrar.fx.mvc.view;
 
+import javafx.event.Event;
+import javafx.event.EventHandler;
+
 public interface View<T> {
-    void show();
-    void close();
-    boolean isShowing();
     T getController();
+    void show();
+    void hide();
+    boolean isShowing();
+    void setOnShow(EventHandler<Event> handler);
+    void setOnHide(EventHandler<Event> handler);
+    void setOnHideRequest(EventHandler<Event> handler);
 }
